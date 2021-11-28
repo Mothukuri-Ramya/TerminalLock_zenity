a0ba0ba4984d591e84db58c139007c274a87e116
Mothukuri-Ramya <85511761+Mothukuri-Ramya@users.noreply.github.com>
2021-11-28 20:45:15 +0530

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
-read -s -p "Enter Terminal password:" password
+which zenity > /dev/null
+if [ $? == 0 ]
+then
+echo "Zenity already installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then 
+sudo apt-get update -y
+sudo apt-get install -y zenity
+else
+echo "Check your Internet Connection"
+fi
+fi
+which figlet > /dev/null
+if [ $? == 0 ]
+then 
+echo "Figlet already Installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then
+sudo apt-get update -y
+sudo apt-get install figlet
+else
+echo "Check your Internet connection"
+fi
+fi
+which md5sum > /dev/null
+if [ $? == 0 ]
+then
+echo "md5sum already installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then
+sudo apt-get update -y
+sudo apt-get install ucommon-utils
+else
+echo "check your internet connection"
+fi
+fi
+password=$(zenity --password --title="Login")
 echo
-sed "s/admin/$password/g" terminallock.sh > TerminalLock
-rm terminallock.sh
+sed "s/ramya/`md5sum <<< $password`/g" terminallock.sh > TerminalLock
+sudo rm terminallock.sh
 sudo chmod +x TerminalLock
 sudo mv TerminalLock /usr/bin/
 sudo echo TerminalLock >> $HOME/.bashrc
