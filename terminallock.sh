7232a8560854f076b27a20d555b5c106ccd8d743
Mothukuri-Ramya <85511761+Mothukuri-Ramya@users.noreply.github.com>
2021-11-28 21:08:17 +0530

    Update terminallock.sh

--- terminallock.sh
+++ terminallock.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 echo
 password=$(zenity --password --title="Login")
-`md5sum <<< $password` == "ramya"
+if [[ `md5sum <<< $password` == "ramya" ]]
+then
+figlet "welcome"
+echo
+figlet "            ramya"
+else
+figlet "who are you"
+sleep 3
+kill $(pgrep terminal)
+fi
+
 
 
